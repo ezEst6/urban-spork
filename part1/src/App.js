c3b87a0edaa9d50259a8e3031d3b793aa7fede70
ezEst <estiga27@gmail.com>
2022-08-04 22:01:53 -0300

    part component added

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,33 @@
 //Los componentes siempre fuera de otros componenttes
 import './App.css';
-import Message from './Mensaje';
+import Header from './Header';
+import Part from './Part';
 
-const Description = () => {
-	return <p>Esta es la app del curso de fullstack bootcamp</p>;
+let ac = 0;
+
+const Content = () => {
+	ac += 10;
+	ac += 7;
+	ac += 14;
+	return (
+		<div>
+			<Part number="10" name="Fundamentals of React" />
+			<Part number="7" name="Using props to pass data" />
+			<Part number="14" name="State of a component" />
+		</div>
+	);
+};
+
+const Total = () => {
+	return <p>Number of exercises {ac}</p>;
 };
 
 function App() {
 	return (
 		<div className="App">
-			<Message msg="Estamos reutilizando" />
-			<Message msg="la misma funcion" />
-			<Description />
+			<Header title="Half Stack application development" />
+			<Content />
+			<Total />
 		</div>
 	);
 }
--- part1/src/Mensaje.js
+++
@@ -1,5 +0,0 @@
-const Message = (props) => {
-	return <h1>{props.msg}</h1>;
-};
-
-export default Message;
